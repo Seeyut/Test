461e2971864ac1872d03a40b559c3a14a201f3dc
Steven Gee <seeyut1102@gmail.com>
2021-08-30 17:32:59 -0700

    Updated code

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -12,30 +12,25 @@ namespace Test {
 
         [Test]
         public void Register() {
-            IWebDriver driver = new FirefoxDriver();
-            driver.Navigate().GoToUrl( "https://moneygaming.qa.gameaccount.com/" );
-            driver.Manage().Window.Maximize();
-            IWebElement joinButton = driver.FindElement( By.XPath( "/html/body/div[2]/div[1]/div[2]/div[1]/a[2]" ) );
-            joinButton.Click();
-            IWebElement titleDropdownList = driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[1]/select[1]" ) );
-            titleDropdownList.Click();
-            SelectElement element = new SelectElement( titleDropdownList );
-            element.SelectByValue( "Mr" );
-            IWebElement firstName = driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[1]/input[1]" ) );
-            IWebElement surName = driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[1]/input[2]" ) );
-            firstName.SendKeys( "Steven" );
-            surName.SendKeys( "Gee" );
-            IWebElement tickBox = driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[5]/input[3]" ) );
-            tickBox.Click();
-            IWebElement joinButton1 = driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[6]/input" ) );
-            joinButton1.Click();
-            IWebElement validationDOB = driver.FindElement( By.CssSelector( "#signupForm > fieldset:nth-child(1) > label:nth-child(15)" ) );
-            string actualError = validationDOB.Text;
-            string expectedError = "This field is required";
-            Assert.AreEqual( actualError, expectedError );
-            Console.WriteLine( "'This field is required' error message appears under date of birth box. " );
-            Console.WriteLine( "Test: PASS " );
-            driver.Close();
+                IWebDriver driver = new FirefoxDriver();
+                driver.Navigate().GoToUrl( "https://moneygaming.qa.gameaccount.com/" );
+                driver.Manage().Window.Maximize();
+                driver.FindElement( By.XPath( "/html/body/div[2]/div[1]/div[2]/div[1]/a[2]" ) ).Click();
+                IWebElement titleDropdownList = driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[1]/select[1]" ) );
+                titleDropdownList.Click();
+                SelectElement element = new SelectElement( titleDropdownList );
+                element.SelectByValue( "Mr" );
+                driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[1]/input[1]" ) ).SendKeys( "Steven" );
+                driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[1]/input[2]" ) ).SendKeys( "Gee" );
+                driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[5]/input[3]" ) ).Click();
+                driver.FindElement( By.XPath( "/html/body/div[2]/div[3]/form/fieldset[6]/input" ) ).Click();
+                IWebElement validationDOB = driver.FindElement( By.CssSelector( "#signupForm > fieldset:nth-child(1) > label:nth-child(15)" ) );
+                string actualError = validationDOB.Text;
+                string expectedError = "This field is required";
+                Assert.AreEqual( actualError, expectedError );
+                Console.WriteLine( "'This field is required' error message appears under date of birth box. " );
+                Console.WriteLine( "Test: PASS " );
+                driver.Close();
         }
     }
 }
